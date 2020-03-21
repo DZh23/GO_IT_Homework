933faef5d8ff0e6a37616fc21f64d3455f9816d7
DZh23 <46024003+DZh23@users.noreply.github.com>
2020-03-21 20:32:53 +0200

    Update module_5_hm.js

--- module_5_hm.js
+++ module_5_hm.js
@@ -74,34 +74,138 @@
 // addItem(item) - получает новый товар и добавляет его к текущим
 // removeItem(item) - получет товар и, если он есть, удаляет его из текущих
 
-class Storage {
-    constructor(items) {
-        this.items = items;
+// class Storage {
+//     constructor(items) {
+//         this.items = items;
+//     }
+//     getItems() {
+//         return this.items;
+//     }
+//     addItem(item) {
+//         this.items.push(item);
+//     }
+//     removeItem(item) {
+//         for (const product of this.items) {
+//             if (product === item) {
+//                 return this.items.splice(this.items.indexOf(product), 1)
+//                 // console.log(this.items.indexOf(product))
+//             }
+//         }
+//     }
+// }
+
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор']);
+// console.log(storage);
+
+
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem('Дроид');
+// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem('Пролонгер');
+// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+
+
+
+// ================================  Задание 4 ==========================================
+// ======================================================================================
+
+
+
+
+// Напиши класс StringBuilder. На вход он получает один параметр - строку, которую записывает в свойство _value.
+
+// Добавь классу следующий функционал:
+
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
+// Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
+
+
+
+class StringBuilder {
+    constructor(str) {
+        // this.str = str;
+        // console.log(this.str);
+        const _value = str;
+        // console.log(_value);
+        this._value = _value;
+        // console.log(this._value);
     }
-    getItems() {
-        return this.items;
+
+    get value() {
+        return (this._value = this._value);
     }
-    addItem(item) {
-        this.items.push(item);
+
+    append(str) {
+        return (this._value = `${this._value}${str}`);
     }
-    removeItem(item) {
-        this.items.push(item);
-        if (this.items.includes('Пролонгер')) {
-            this.items.pop(item);
-        }
+    prepend(str) {
+        return (this._value = `${str}${this._value}`);
+        // console.log(`Prepend: `, this._value);
     }
+    
+    pad(str) {
+        return (this._value = `${str}${this._value}${str}`);
+    }
+
 }
 
-const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор']);
-console.log(storage);
+
+
+const builder = new StringBuilder('.');
+console.log(builder);
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
 
 
 
-const items = storage.getItems();
-console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+// TASK 4
 
-storage.addItem('Дроид');
-console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+// class StringBuilder {
+//   constructor(str) {
+//     this.str = str;
+//     const _value = str;
+//     this._value = _value;
+//   }
 
-storage.removeItem('Пролонгер');
-console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+//   get value() {
+//     return (this._value = this._value);
+//   }
+
+//   append(str) {
+//     return (this._value = `${this._value}${str}`);
+//   }
+
+//   prepend(str) {
+//     return (this._value = `${str}${this._value}`);
+//   }
+
+//   pad(str) {
+//     return (this._value = `${str}${this._value}${str}`);
+//   }
+// }
+
+// const builder = new StringBuilder(".");
+
+// builder.append("^");
+// console.log(builder.value); // '.^'
+
+// builder.prepend("^");
+// console.log(builder.value); // '^.^'
+
+// builder.pad("=");
+// console.log(builder.value); // '=^.^='
